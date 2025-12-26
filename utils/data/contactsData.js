38f298b19c7b8fa7ca88bec38cfd6e9897705968
Shree <shreemengshetti12@gmail.com>
2025-12-26 22:44:37 +0530

    first

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+  email: "shreemengshetti12@gmail.com",
+  phone: "+91 7710860509",
+  address: "Navi Mumbai, Maharashtra, India",
+  github: "https://github.com/shreemengshetti2005",
+  //   facebook: "https://www.facebook.com/abusaid.riyaz/",
+  linkedIn: "https://www.linkedin.com/in/shree-mengshetti-0a91b0217/",
+  //   twitter: "https://twitter.com/said7388",
+  //   stackOverflow: "https://stackoverflow.com/users/16840768/abu-said",
+  devUsername: "shreemengshetti2005",
+};
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "shree mengshetti",
+  profile: "/profile.png",
+  designation: "Software Developer",
+  description:
+    "My name is Shree Mengshetti. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "shreemengshetti12@gmail.com",
+  phone: "+8801608797655",
+  address: "Navi Mumbai, Maharashtra, India",
+  github: "https://github.com/shreemengshetti2005",
+  linkedIn: "https://www.linkedin.com/in/shree-mengshetti-0a91b0217/",
+  codechef: "https://www.codechef.com/users/shree_meng",
+  codeforces: "https://codeforces.com/",
+  devUsername: "shreeMengshetti2005",
+  resume:
+    "https://drive.google.com/file/d/102C9EPsTNkSqlUshZlIay_GmPeVgUxu1/view?usp=sharing",
+};
